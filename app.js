a9ba4fbbee04ef572e3b1f3810f6411620b028dd
Christian Gärtner <christiangaertner.film@googlemail.com>
2013-11-03 20:05:58 +0100

    Improving wrapper

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
 var sys = require('sys'),
-spawn = require('child_process').spawn,
+spawn = require('child_process').spawn;
 
+var DEBUG = false;
 
+var cmdArgs = process.argv.splice(2);
 
-// data = [['Label', 'Value'], ['Foo', 2], ['Bar', 1], ['Baz', 1]], // gauge
-data = [['Year', 'Players', 'DownTimes'], ['2004', 1000, 20], ['2005', 1170, 50], ['2006', 660, 1], ['2007', 50, 23]], // linechart
-// data = [['Task', 'Hours per Day'], ['Work', 11], ['Eat', 2], ['Commute', 2], ['Watch TV', 2], ['Sleep', 7]], // piechart
-arguments = JSON.stringify(data);
+if (cmdArgs[0] == 'debug' || cmdArgs[0] == 'DEBUG') {
+	DEBUG = true;
+}
 
+var render = function(name, type, width, height, arguments) {
+	var prc = spawn('phantomjs', ['render.js', name, type, width, height, arguments]);
+	prc.stdout.setEncoding('utf8');
 
-var prc = spawn('phantomjs', ['render.js', 'name', 'linechart', 500, 500, arguments]);
-prc.stdout.setEncoding('utf8');
+	if (DEBUG) {
+		prc.stdout.on('data', function(data) {
+			var str = data.toString()
+		    var lines = str.split(/(\r?\n)/g);
+		    console.log('>>\n' + lines.join("") + '\n<<');
+		});
 
-prc.stdout.on('data', function(data) {
-	var str = data.toString()
-    var lines = str.split(/(\r?\n)/g);
-    console.log('>>\n' + lines.join("") + '\n<<');
-});
+		prc.on('close', function (code) {
+		    console.log('process exit code ' + code);
+		});
+	}
+	
+};
 
-prc.on('close', function (code) {
-    console.log('process exit code ' + code);
-});
+var allData = [
+		[['Year', 'Players', 'DownTimes'], ['2004', 1000, 20], ['2005', 1170, 50], ['2006', 660, 1], ['2007', 50, 23]],
+		[['Year', 'Players', 'DownTimes'], ['2004', 1000, 0], ['2005', 1170, 20], ['2006', 660, 2], ['2007', 1030, 2]],
+		[['Year', 'Players', 'DownTimes'], ['2004', 1000, 0], ['2005', 1170, 40], ['2006', 660, 3], ['2007', 1030, 55]],
+		[['Year', 'Players', 'DownTimes'], ['2004', 1000, 0], ['2005', 1170, 10], ['2006', 660, 55], ['2007', 1030, 44]]
+	];
+
+for (var i = allData.length - 1; i >= 0; i--) {
+	arguments = JSON.stringify(allData[i]);
+	render('img' + i, 'linechart', 500, 500, arguments);
+};
