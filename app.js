ff3b7cfc12056b352756fd439c6d4062d1f1f124
Christian Gärtner <christiangaertner.film@googlemail.com>
2013-11-04 16:36:43 +0100

    Improved wrapper and added time measurement

--- app.js
+++ app.js
@@ -1,40 +1,47 @@
 var sys = require('sys'),
 spawn = require('child_process').spawn;
 
-var DEBUG = false;
+/**
+ * Time Measurement
+ * Expected Times:
+ * (Tested on iMac 3.06GHz Intel Core i3; 12GB 1067 MHz DDR 3 RAM; OS X 10.8.5)
+ * 01 image : 02185ms => 02.185s
+ * 10 images: 06876ms => 06.876s
+ * 20 images: 11799ms => 11.799s
+ * 50 images: 28367ms => 28.367s
+ * 80 images: 46380ms => 46.380s
+ * 81 images: FAILED.
+ */
+process.on('exit', function() {
+	console.timeEnd("Rendering");	
+});
 
-var cmdArgs = process.argv.splice(2);
+var imgCount = 10;
 
-if (cmdArgs[0] == 'debug' || cmdArgs[0] == 'DEBUG') {
-	DEBUG = true;
-}
 
 var render = function(name, type, width, height, arguments) {
+	console.log('Starting Rendering: ' + name);
 	var prc = spawn('phantomjs', ['render.js', name, type, width, height, arguments]);
-	prc.stdout.setEncoding('utf8');
+	// prc.stdout.setEncoding('utf8');
+	// prc.stdout.on('data', function(data) {
+	// 	var str = data.toString()
+	//     var lines = str.split(/(\r?\n)/g);
+	//     console.log('>>\n' + lines.join("") + '\n<<');
+	// });
 
-	if (DEBUG) {
-		prc.stdout.on('data', function(data) {
-			var str = data.toString()
-		    var lines = str.split(/(\r?\n)/g);
-		    console.log('>>\n' + lines.join("") + '\n<<');
-		});
-
-		prc.on('close', function (code) {
-		    console.log('process exit code ' + code);
-		});
-	}
-	
+	prc.on('close', function (code) {
+		    console.log('Finished rendering of ' + name + ' | Status: ' + code);
+	});	
 };
 
-var allData = [
-		[['Year', 'Players', 'DownTimes'], ['2004', 1000, 20], ['2005', 1170, 50], ['2006', 660, 1], ['2007', 50, 23]],
-		[['Year', 'Players', 'DownTimes'], ['2004', 1000, 0], ['2005', 1170, 20], ['2006', 660, 2], ['2007', 1030, 2]],
-		[['Year', 'Players', 'DownTimes'], ['2004', 1000, 0], ['2005', 1170, 40], ['2006', 660, 3], ['2007', 1030, 55]],
-		[['Year', 'Players', 'DownTimes'], ['2004', 1000, 0], ['2005', 1170, 10], ['2006', 660, 55], ['2007', 1030, 44]]
-	];
+var allData = [];
 
-for (var i = allData.length - 1; i >= 0; i--) {
+for (var i = 0; i < imgCount; i++) {
+	allData.push([['Year', 'Players', 'DownTimes'], ['2004', 1000, 20], ['2005', 1170, 50], ['2006', 660, 1], ['2007', 50, 23]]);
+};
+
+console.time("Rendering");
+for (var i = 0; i < allData.length; i++) {
 	arguments = JSON.stringify(allData[i]);
-	render('img' + i, 'linechart', 500, 500, arguments);
+	render('img' + i, 'linechart', 500, 500, arguments, i);
 };
