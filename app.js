ff4e769b28b33cee973c879431cf5f36bb0de1a7
Christian Gärtner <christiangaertner.film@googlemail.com>
2013-11-08 20:27:15 +0100

    Further improvements

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ async = require('async');
 var imgCount = 3;
 var maxCon = 1;
 
-var render = function(data, callback) {
-	console.log('Starting Rendering: ' + data.name);
-	
-	spawn('phantomjs', ['render.js', data.name, data.type, data.width, data.height, data.arguments])
+var render = function(name, type, width, height, arguments, callback) {
+	spawn('phantomjs', ['render.js', name, type, width, height, arguments])
 	.on('close', function (code) {
-		console.log('Finished rendering of ' + data.name + ' | Status: ' + code);
-		callback();
+		if (code == 0) {
+			callback(null, name);
+		} else {
+			callback(new Error('Error in PhantomJS process. Exit Code: ' + code), name);
+		}
 	});
 };
 
@@ -20,7 +21,16 @@ for (var i = 0; i < imgCount; i++) {
 	allData.push([['Year', 'Players', 'DownTimes'], ['2004', 1000, 20], ['2005', 1170, 50], ['2006', 660, 1], ['2007', 50, 23]]);
 };
 
-var queue = async.queue(render, maxCon);
+var queue = async.queue(function(data, callback) {
+	console.log('Starting Rendering: ' + data.name);
+	
+	render(data.name, data.type, data.width, data.height, data.arguments, function(err, name) {
+		if (err) console.log('Failed to Render: ' + name  + '>> '+ err);
+		console.log('Finished rendering of ' + name);
+		callback();
+	});
+}, maxCon);
+
 queue.drain = function() {
 	console.timeEnd("Rendering");
 }
